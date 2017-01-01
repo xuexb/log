6dc035bbb95865696eebde112ec908cedf7fdea0
xuexb <fe.xiaowu@gmail.com>
2017-01-01 12:03:41 +0800

    添加注释

--- src/index.js
+++ src/index.js
@@ -82,19 +82,37 @@
          */
         _sendImg: function (str) {
             var key = Log.expando + (Log.guid++);
+
+            // 这里一定要挂在window下
+            // 在IE中，如果没挂在window下，这个img变量又正好被GC的话，img的请求会abort
+            // 导致服务器收不到日志
             var img = window[key] = new Image();
 
-            img.onload = img.onerror = function () {
-                img.onload = img.onerror = null;
+            img.onload = img.onerror = img.onabort = function () {
+                // 下面这句非常重要
+                // 如果这个img很不幸正好加载了一个存在的资源，又是个gif动画
+                // 则在gif动画播放过程中，img会多次触发onload
+                // 因此一定要清空
+                img.onload = img.onerror = img.onabort = null;
 
+                // 清空window变量
                 try {
                     delete window[key];
                 }
                 catch (e) {
                     window[key] = null;
                 }
+
+                // 下面这句非常重要
+                // new Image创建的是DOM，DOM的事件中形成闭包环引用DOM是典型的内存泄露
+                // 因此这里一定要置为null
+                img = null;
             };
 
+            // 一定要在注册了事件之后再设置src
+            // 不然如果图片是读缓存的话，会错过事件处理
+            // 最后，对于url最好是添加客户端时间来防止缓存
+            // 同时服务器也配合一下传递Cache-Control: no-cache;
             img.src = this._options.img + str;
         }
     });
