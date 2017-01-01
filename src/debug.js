988f1d64acd4ee94a508ccff5f8641308d7c0574
xuexb <fe.xiaowu@gmail.com>
2017-01-01 19:02:55 +0800

    update debug

--- src/debug.js
+++ src/debug.js
@@ -1,11 +1,12 @@
 /**
  * @file 日志debug模块
  * @author xiaowu <fe.xiaowu@gmail.com>
- * @requires ./index.js
  */
 
 (function (window, $, Log) {
     'use strict';
+
+    var debug = {};
 
     /**
      * 标识
@@ -15,7 +16,7 @@
      *     2 - 打印所有信息
      * @type {string}
      */
-    var debug = (function () {
+    debug.type = (function () {
         var value = (location.search.substr(1).match(/(?:^|&)log\.debug=(.+?)(?:$|&)/) || ['', '1'])[1];
 
         // 如果不在白名单
@@ -26,26 +27,78 @@
         return value;
     })();
 
-    var mock = function (name, callback) {
-        var old = Log[name];
+    /**
+     * 输出日志信息
+     *
+     * @param  {any} data   输出数据
+     * @param  {string} [level=error] 类型, 如果是error则都输出, 如果为warning则只在[打印全部信息]时输出
+     */
+    debug.log = function (data, level) {
+        if (level === 'error' || !level) {
+            console.error(data);
+        }
+        else if (level === 'warning' && debug.type === '2') {
+            console.warn(data);
+        }
 
-        Log[name] = function () {
+    };
+    if (!window.console) {
+        debug.log = $.noop || function () {};
+    }
+
+    /**
+     * 拦截数据
+     *
+     * @param  {string}   name     名字
+     * @param  {Function} callback 回调, 参数1是args, 参数2是下一步
+     */
+    debug.mock = function (name, callback) {
+        var old;
+        var type = name.split('.')[0].toLowerCase();
+        var fn = function () {
+            var context = this;
             var args = [].slice.call(arguments);
 
-            callback(args, function () {
-                old.apply(old, args);
-            });
+            callback.apply(null, args);
+
+            return old.apply(context, args);
         };
+
+        name = name.split('.')[1];
+
+        if (type === 'log') {
+            old = Log[name];
+            Log[name] = fn;
+        }
+        else if (type === 'class') {
+            old = Log.Class.prototype[name];
+            Log.Class.prototype[name] = fn;
+        }
+
     };
 
     // Log.create
-    mock('create', function (args, next) {
-        var options = args[0];
+    debug.mock('Log.create', function (options) {
+        if (!options) {
+            debug.log('Log.create options 不能为空');
+        }
+        else if (!$.isPlainObject(options)) {
+            debug.log('Log.create options 不为对象');
+        }
+        else if (!options.img) {
+            debug.log('Log.create options.img 不能为空');
+        }
+        else if (!options.global) {
+            debug.log('Log.create options.global 全局参数为空', 'warning');
+        }
 
     });
+
+    // Class.send
+    debug.mock('Class.send', function (options) {});
 
     // 幅值到全局对象上
     Log.debug = debug;
 
-    console.info('日志调试模式: ' + ['', '只报错误', '显示全部信息'][debug]);
+    console.info('日志调试模式: ' + ['', '只报错误', '显示全部信息'][debug.type]);
 })(window, window.$, window.Log);
