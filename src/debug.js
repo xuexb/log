9c2f1d32a2022f46aa2cda825ce8582e7943be44
xuexb <fe.xiaowu@gmail.com>
2017-03-11 17:54:10 +0800

    修改debug.js, 改为只校验参数是否为空, 超长

--- src/debug.js
+++ src/debug.js
@@ -6,175 +6,59 @@
 (function (window, $, Log) {
     'use strict';
 
+    var debug = Log.debug = {};
+
     /**
-     * 验证参数
+     * 获取完成链接, 如果是以 / 开头, 则匹配当前的链接
      *
-     * @param  {string} value 日志的参数值
-     * @param  {Object} data  规则数据
+     * @param  {string} url 链接
      *
-     * @return {boolean}       是否通过
+     * @return {string}
      */
-    var validateParam = function (value, data) {
-        var type = data.type.toLowerCase();
-        var flag = false;
-
-        // 如果是正则
-        if (type === 'regexp' ) {
-            flag = new RegExp(data.value.replace('\\', '\\\\')).test(value);
+    var getLogPath = function (url) {
+        if (String(url)[0] === '/') {
+            url = location.origin + url;
         }
-
-        // 如果是数组则是包含就算
-        else if (type === 'array') {
-            // 因为indexOf不是隐式转换, 而值肯定是string, 这里先把数字转字符
-            $.each(data.value.split(/\s*,\s*/), function (index, key) {
-                if (key === value + '') {
-                    flag = true;
-                    return false;
-                }
-            });
-        }
-
-        // 否则全比较
-        else {
-            flag = value === data.value + '';
-        }
-
-        return flag;
-    };
-
-    var obj2Arr = function (obj) {
-        var arr = [];
-        $.each(obj, function (key, val) {
-            arr.push($.extend({
-                name: key
-            }, val));
-        });
-        return arr;
+        return url;
     };
 
     /**
-     * 验证
+     * 日志链接最大长度
      *
-     * @description
-     *     1. 必选 + 无值 => false
-     *     2. 必选 + 有值 => check
-     *     3. 非必选 + 无值 => true
-     *     4. 非必选 + 有值 => check
-     * @param  {Object} target 参数对象
-     *
-     * @return {Object}
+     * @type {number}
      */
-    var validate = function (target) {
-        var data = debug.param;
-        var pass = {};
-        var error = {};
-        var add = {};
-
-        // 先根据规则验证一遍
-        $.each(data, function (key, val) {
-            var param = target[key];
-            var isNull = !target.hasOwnProperty(key);
-            var result = $.extend({}, val, {
-                param: param
-            });
-
-            // 如果必选且没有参数
-            if (val.required && isNull) {
-                result.status = 'error';
-                error[key] = result;
-                console.log('必须没有参数')
-            }
-
-            // 如果不为必选, 且没有参数则忽略
-            else if (!val.required && isNull) {
-                return;
-            }
-
-            else {
-                if (validateParam(param, val)) {
-                    result.status = 'pass';
-                    pass[key] = result;
-                }
-                else {
-                    result.status = 'error';
-                    error[key] = result;
-                }
-            }
-
-        });
-
-        // 检查新增参数
-        $.each(target, function (key) {
-            // 如果已经处理过
-            if (pass[key] || error[key] || add[key]) {
-                return;
-            }
-
-            add[key] = {
-                name: key,
-                param: target[key],
-                status: 'add'
-            };
-        });
-
-        error = obj2Arr(error);
-        add = obj2Arr(add);
-        pass = obj2Arr(pass);
-
-        return {
-            add: add,
-            error: error,
-            pass: pass,
-            all: [].concat(error, add, pass)
-        };
-    };
-
-    var debug = Log.debug || (Log.debug = {});
+    debug.URL_MAX_LENGTH = 100;
 
     /**
-     * 标识
+     * 参数值最大长度
      *
-     * @example
-     *     1 - 默认, 只报严重错误
-     *     2 - 打印所有信息
-     * @type {string}
+     * @type {number}
      */
-    debug.type = (function () {
-        var value = (location.search.substr(1).match(/(?:^|&)log\.debug=(.+?)(?:$|&)/) || ['', '1'])[1];
-
-        // 如果不在白名单
-        if ($.inArray(value, ['1', '2']) === -1) {
-            value = '1';
-        }
-
-        return value;
-    })();
+    debug.PARAM_MAX_LENGTH = 10;
 
     /**
-     * 输出日志信息
-     *
-     * @param  {any} data   输出数据
-     * @param  {string} [level=error] 类型, 如果是error则都输出, 如果为warning则只在[打印全部信息]时输出
+     * 报错
      */
-    debug.log = function (data, level) {
-        if (level === 'error' || !level) {
-            console.error(data);
-            // throw new TypeError(data);
+    debug.error = function () {
+        if (window.console && window.console.error) {
+            console.error.apply(console, [].slice.call(arguments));
         }
-        else if (level === 'warning' && debug.type === '2') {
-            console.warn(data);
-        }
-
     };
-    if (!window.console) {
-        debug.log = $.noop || function () {};
-    }
+
+    /**
+     * 警告
+     */
+    debug.warn = function () {
+        if (window.console && window.console.warn) {
+            console.warn.apply(console, [].slice.call(arguments));
+        }
+    };
 
     /**
      * 拦截数据
      *
      * @param  {string}   name     名字
-     * @param  {Function} callback 回调, 参数1是args, 参数2是下一步
+     * @param  {Function} callback 回调, 参数是调用时的参数, 只执行回调, 不阻止代码
      */
     debug.mock = function (name, callback) {
         var old;
@@ -201,74 +85,63 @@
 
     };
 
-    // Log.create
-    debug.mock('Log.create', function (options) {
-        if ('undefined' === typeof options) {
-            debug.log('Log.create() 不能为空');
+    // 创建时校验参数
+    debug.mock('Log.create', function (url, options) {
+        if ('undefined' === typeof url) {
+            debug.error('Log.create(url) - url不能为空');
         }
-        else if (!$.isPlainObject(options)) {
-            debug.log('Log.create(' + String(options) + ') 不为对象');
+        else if ('string' !== typeof url || !url) {
+            debug.error('Log.create(url) - url必须为非空字符串');
         }
-        else if (!options.img) {
-            debug.log('Log.create({img}) 不能为空');
+        else if (options && !$.isPlainObject(options)) {
+            debug.error('Log.create(url, options) - options必须为对象');
         }
-        else if (!options.global) {
-            debug.log('Log.create(options) 全局参数options.global为空', 'warning');
-        }
-
     });
 
     // 检测全局覆盖时有没有参数重复
+    // 检测空参数
+    // 检测超长参数
     debug.mock('Class._makeGlobal', function (data) {
-        var options = this._options;
+        var options = this.global || {};
+        var params = {};
 
-        $.each(options.global, function (key) {
+        // 循环全局参数, 判断send()参数里存在则报警
+        $.each(options, function (key, value) {
+            // global里支持fn
+            if ($.isFunction(value)) {
+                value = value();
+            }
+
             if (data.hasOwnProperty(key)) {
-                debug.log([
-                    'send里存在全局global里重复参数"' + key + '", ',
-                    '参数值将由"' + data[key] + '"被覆盖为全局的"' + options.global[key] + '"'
-                ].join(''), 'warning');
+                debug.warn([
+                    'send里存在全局global里重复参数 "' + key + '" , ',
+                    '参数值将由全局的 "' + value + '" 更新为 "' + data[key] + '"'
+                ].join(''));
+            }
+
+            params[key] = value;
+        });
+
+        $.each(data, function (key, value) {
+            params[key] = value;
+        });
+
+        $.each(params, function (key, value) {
+            if ('undefined' === typeof value) {
+                debug.warn('存在空参数: ' + key);
+            }
+            else if (String(value).length > debug.PARAM_MAX_LENGTH) {
+                debug.warn('存在超长参数: ' + key + '=' + value + ', 长度为: ' + String(value).length);
             }
         });
     });
 
-    // Class.send
-    debug.mock('Class.send', function (key, value) {
-        var data = {};
+    // 在发送图片时检测参数
+    debug.mock('Log.sendImg', function (data) {
+        var length = getLogPath(this.url + Log.json2url(data)).length;
 
-        if (!key) {
-            debug.log('send() 不能为空');
-        }
-        else if ($.isPlainObject(key)) {
-            if ($.isEmptyObject(key)) {
-                debug.log('send({}) 出现空参数', 'warning');
-            }
-            else {
-                $.extend(data, key);
-            }
-        }
-        else if ('string' === typeof key && 'undefined' === typeof value) {
-            debug.log('send(key) 值不能为空');
-        }
-        else if ('string' !== typeof key) {
-            debug.log('send(' + key + ') key不为string');
-        }
-        else {
-            data[key] = value;
+        if (length > debug.URL_MAX_LENGTH) {
+            debug.warn('日志链接超长, 长度为: ' + length);
         }
     });
-
-    // Class.sendImg
-    debug.mock('Class.sendImg', function (data) {
-        var result;
-
-        if (debug.param) {
-            result = validate(data);
-            console.log(JSON.stringify(result, null, 4));
-        }
-
-        debug.log(window.JSON ? JSON.stringify(data, null, 4) : data, 'warning', $.param(data));
-    });
-
-    console.info('日志调试模式: ' + ['', '只报错误', '显示全部信息'][debug.type]);
 })(window, window.$, window.Log);
